253247792e02f404445cf1c1132f37cf562c065a
aki <aki@mymail.com>
2023-02-06 17:11:19 +0900

    Luaで書かれたconfigファイルにifで条件分岐を仕込む

---
+++ ss2/config_if.lua
@@ -0,0 +1,7 @@
+-- configuration file for pogram lua_0
+-- define window size
+if os.getenv("DISPLAY") == "yes" then
+    width = 300; height = 300
+else
+    width = 200; height = 200
+end
---
+++ ss2/lua_1.c
@@ -0,0 +1,39 @@
+#include <lua.h>
+#include <lauxlib.h>
+#include <lualib.h>
+#include <wchar.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
+void error (lua_State *L, const char *fmt, ...) {
+    va_list argp;
+    va_start(argp, fmt);
+    vfprintf(stderr, fmt , argp);
+    va_end(argp);
+    lua_close(L);
+    exit(EXIT_FAILURE);
+}
+
+void load_config (char *filename, int *width, int *height) {
+    lua_State *L = luaL_newstate();
+    luaL_openlibs(L);
+
+    if(luaL_loadfile(L,filename) || lua_pcall(L,0,0,0)) {
+        error(L,"cannot run cofiguration file: %s", lua_tostring(L,-1));
+    }
+    lua_getglobal(L,"width");
+    lua_getglobal(L,"height");
+    if(!lua_isnumber(L,-2))
+        error(L,"`width` should be a number: %s", lua_tostring(L,-1));
+    if(!lua_isnumber(L,-1))
+        error(L,"`height` should be a number: %s", lua_tostring(L,-1));
+    *width = (int)lua_tonumber(L, -2); 
+    *height = (int)lua_tonumber(L, -1); 
+}
+
+int main(void) {
+    int width = 0, height = 0;
+    load_config("config_if.lua",&width, &height);
+    printf("widht = %d, height = %d\n", width, height);
+}
