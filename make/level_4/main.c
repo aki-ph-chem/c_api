0ec651a8d1f75353933ff99b4827df8a0273a17f
aki <aki@mymail.com>
2023-02-09 17:27:33 +0900

    level 4

---
+++ make/level_4/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <greet.h> 
+#include <add.h>
+
+int main(void) {
+    greet("Aki");
+    int x = 1100;
+    int y = 11;
+    printf("x + y = %d\n"
+            ,add(x,y));
+}
